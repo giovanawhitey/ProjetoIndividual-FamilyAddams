543378f51fd52609b9ba43212ba6dc77b6a23b2c
gibr4nquinho <giovana.branquinho@sptech.school>
2025-11-23 02:08:33 -0300

    Alterações app js

--- Site/app.js
+++ Site/app.js
@@ -1,40 +1,41 @@
+
 // var ambiente_processo = 'producao';
 var ambiente_processo = 'desenvolvimento';
 
 var caminho_env = ambiente_processo === 'producao' ? '.env' : '.env.dev';
-// Acima, temos o uso do operador ternário para definir o caminho do arquivo .env
-// A sintaxe do operador ternário é: condição ? valor_se_verdadeiro : valor_se_falso
-
 require("dotenv").config({ path: caminho_env });
 
 var express = require("express");
 var cors = require("cors");
 var path = require("path");
-var PORTA_APP = process.env.APP_PORT;
-var HOST_APP = process.env.APP_HOST;
+
+var PORTA_APP = process.env.APP_PORT ;
+var HOST_APP = process.env.APP_HOST ;
 
 var app = express();
 
-// --- ROTAS DO SEU PROJETO ---
 var indexRouter = require("./src/routes/index");
 var usuarioRouter = require("./src/routes/usuarios");
 var quizRouter = require("./src/routes/quiz");
 var dashboardRouter = require("./src/routes/dashboard");
-// ------------------------------
+var caracteristicaRouter = require("./src/routes/caracteristica");
+var resultadoRouter = require("./src/routes/resultado");
+var tentativaRouter = require("./src/routes/tentativa"); 
 
-// CONFIGURAÇÕES PADRÃO
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
 
-// USO DAS ROTAS
 app.use("/", indexRouter);
 app.use("/usuarios", usuarioRouter);
 app.use("/quiz", quizRouter);
 app.use("/dashboard", dashboardRouter);
+app.use("/caracteristica", caracteristicaRouter);
+app.use("/resultado", resultadoRouter);
+app.use("/tentativa", tentativaRouter);
 
-// INICIAR SERVIDOR
+
 app.listen(PORTA_APP, function () {
     console.log(`
     ##   ##  ######   #####             ####       ##     ######     ##              ##  ##    ####    ######  

1067dc29723c71f0ec8cc287dc453dd09e383661
gibr4nquinho <giovana.branquinho@sptech.school>
2025-11-28 15:42:34 -0300

    🖤 Projeto Finalizado 🖤

--- Site/src/models/resultadoModel.js
+++ Site/src/models/resultadoModel.js
@@ -1,5 +1,34 @@
 var database = require("../database/config");
 
+function obterDadosUsuario(req, res) {
+    console.log("Obtendo dados da dashboard do usuário");
+
+    var idUsuario = req.params.idUsuario;
+
+    var sqlCaracteristicas = `
+        SELECT C.nomeCaracteristica, COUNT(R.idResultado) AS votos
+        FROM Resultado R
+        JOIN Caracteristica C ON R.fkCaracteristica = C.idCaracteristica
+        WHERE R.fkUsuario = ${idUsuario}
+        GROUP BY C.nomeCaracteristica
+        ORDER BY votos DESC
+    `;
+
+    var sqlPersonagens = `
+        SELECT Q.nome, COUNT(R.idResultado) AS votos
+        FROM Resultado R
+        JOIN Quiz Q ON R.fkQuiz = Q.idQuiz
+        WHERE R.fkUsuario = ${idUsuario}
+        GROUP BY Q.nome
+        ORDER BY votos DESC
+    `;
+
+    var sqlTentativas = `
+        SELECT COUNT(*) AS totalTentativas
+        FROM Resultado
+        WHERE fkUsuario = ${idUsuario}
+    `;
+}
 function listar() {
     var instrucaoSql = `
         SELECT 
@@ -81,5 +110,4 @@ module.exports = {
     buscarResultadoFkUsuario,
     personagemUser,
     caracteristicaUser, 
-    buscarUltimoPersonagem
 };
